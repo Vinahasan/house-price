79086aed7265b0b7387c8a7e76ad8bf3fb3bb025
Vinahasan <113814241+Vinahasan@users.noreply.github.com>
2022-12-02 15:01:43 +0700

    Update houseprice.py

--- houseprice.py
+++ houseprice.py
@@ -10,7 +10,7 @@ train = pd.read_csv("saleprice.csv")
 st.title("Predict Your Dream House Here!")
 
 #pick variables
-train_a=train[['OverallQual', 'GrLivArea', 'GarageCars', 'SalePrice']].copy()
+train_a=train[['OverallQual', 'GrLivArea', 'GarageCars', 'LotArea', 'OverallCond', 'BedroomAbvGr', 'KitchenAbvGr', 'SalePrice']].copy()
 train_a.info()
 
 # Pembagian X dan y
@@ -20,14 +20,17 @@ y= train_a['SalePrice']
 
 ## Splitting X and Y into training and testing data
 from sklearn.model_selection import train_test_split
-X_train, y_test, y_train, X_test = train_test_split(X,y, test_size=0.4,
+X_train, y_test, y_train, X_test = train_test_split(X,y, test_size=0.3,
                                                     random_state=1)
 
 #input the numbers
 OverallQual = st.number_input("Overall Quality",int(X_train.OverallQual.min()),int(X_train.OverallQual.max()),int(X_train.OverallQual.mean()))
-GrLivArea = st.number_input("Grade Living Area",int(train_a.GrLivArea.min()),int(train_a.GrLivArea.max()),int(train_a.GrLivArea.mean()))
+GrLivArea = st.number_input("Living Area",int(train_a.GrLivArea.min()),int(train_a.GrLivArea.max()),int(train_a.GrLivArea.mean()))
 GarageCars = st.number_input("Garage Cars",int(train_a.GarageCars.min()),int(train_a.GarageCars.max()),int(train_a.GarageCars.mean()))
-
+LotArea = st.number_input("Lot Area",int(X_train.LotArea.min()),int(X_train.LotArea.max()),int(X_train.LotArea.mean()))
+OverallCond= st.number_input("Overall Condition",int(train_a.OverallCond.min()),int(train_a.OverallCond.max()),int(train_a.OverallCond.mean()))
+BedroomAbvGr= st.number_input("Bedroom",int(train_a.BedroomAbvGr.min()),int(train_a.BedroomAbvGr.max()),int(train_a.BedroomAbvGr.mean()))
+KitchenAbvGr = st.number_input("Kitchen",int(train_a.KitchenAbvGr.min()),int(train_a.KitchenAbvGr.max()),int(train_a.KitchenAbvGr.mean()))
 
 ## Create linear regression object
 from sklearn import linear_model
@@ -37,7 +40,7 @@ reg = linear_model.LinearRegression()
 reg.fit(X_train, y_train)
 
 #Output
-predictions = reg.predict([[OverallQual,GrLivArea,GarageCars]])[0]
+predictions = reg.predict([[OverallQual, GrLivArea, GarageCars, LotArea, OverallCond, BedroomAbvGr, KitchenAbvGr]])[0]
 
 if st.button('Predict'):
     st.header('Your estimated house price will be $ {}'.format(int(predictions)))
